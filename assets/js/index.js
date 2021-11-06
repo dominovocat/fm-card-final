c13d146f3ab199a8cee2641fd4c8d6f13a02d69c
danil <minakonn9shkoni@gmail.com>
2021-11-06 19:20:17 +0300

    Add createElement

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,53 @@
+"use strict";
 
+fetch("./assets/js/data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data)
+    
+  });
 
-function createElement(type,{classNames,typeEvent,onClick}, ...children){
+const cardsContainer = document.getElementById("cardsContainer");
+const HTMLElements = []
+  .filter(
+    (actor) =>
+      actor.firstName &&
+      actor.lastName &&
+      actor.profilePicture &&
+      actor.contacts
+  )
+  .map((actor) => createActorCards(actor));
+
+function createActorCards(actor) {
+  return createElement("li", { classNames: ["cardWrapper"] }, [
+    createElement("article", { classNames: ["cardContainer"] }, [
+      createImageWrapper(actor),
+      createElement("h2", { classNames: ["cardName"] }, [
+        document.createTextNode(actor.firstName || "noname"),
+        document.createTextNode(actor.lastName),
+      ]),
+      createElement("p", { classNames: ["cardDescription"] }, [
+        document.createTextNode(actor.birthdate || "unknow"),
+      ]),
+    ]),
+  ]);
+}
+cardsContainer.append(...HTMLElements);
+
+function createElement(
+  type,
+  { attributes, classNames, typeEvent, onClick, events },
+  ...children
+) {
   const elem = document.createElement(type);
+  for (const [attrName, attrValue] of Object.entries(attributes)) {
+    elem.setAttribute(attrName, attrValue);
+  }
+  for (const [eventType, eventHandler] of Object.entries(events)) {
+    elem.addEventListener(eventType, eventHandler);
+  }
   elem.classList.add(...classNames);
-  elem.addEventListener(typeEvent,onClick);
+  elem.addEventListener(typeEvent, onClick);
   elem.append(...children);
   return elem;
 }
